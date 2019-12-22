dd13e52505e6770232fd98f2223c00b73282ba5d
Shuaishuai Li <lishsh@rd.netease.com>
2019-12-22 13:30:22 +0800

    java-spring-api-helloworld

---
+++ learnSpringJPA/src/main/java/com/aiwebops/learnspringjpa/entities/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.aiwebops.learnspringjpa.entities;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+
+}
